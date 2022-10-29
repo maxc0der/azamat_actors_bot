6767364ccdd7a914ecbf7d1f67d6d30861d213c1
maxc0der <maximka5406@gmail.com>
2022-10-29 14:50:29 +0300

    test_cv.py changed

--- test_cv.py
+++ test_cv.py
@@ -1,14 +1,15 @@
 import numpy as np
 from random import randint
 import cv2
+import os
 
 print(cv2.__version__)
 image = np.random.randint(255, size=(512, 512, 3))
 random_number = randint(100000, 999999)
-path = 'images/1_1.jpeg'
+
+file_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(file_path, 'images/1_1.jpeg')
 print(path)
 val = cv2.imwrite(path, image)
 print(val)
 
-val = cv2.imwrite('images/1_2.jpeg', image[0:100, 0:100])
-print(val)
